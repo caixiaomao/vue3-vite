88aad43596d4942d503af954700763efceb5e588
caixiaomao <532001566@qq.com>
2020-10-20 14:40:40 +0800

    引入router、store

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+// router
+import router from './router'
+import store from './store'
+// ant-design-vue
+import antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(router)
+  .use(store)
+  .use(antd)
+  .mount('#app')
---
+++ src/views/todo/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <a-button type="primary">todo</a-button>
+  </div>
+</template>
+
+<script>
+  export default {
+  name: "todo"
+}
+</script>
+
+<style scoped>
+
+</style>
